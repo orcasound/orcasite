c5dc5be7f9e5ff81170a881f8a4b0142a533e8b6
Paul Cretu <paul.s.cretu@gmail.com>
2018-02-28 06:40:26 +0000

    Added graphql deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule Orcasite.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_plug, "~> 1.4"},
+      {:poison, "~> 3.1.0"} # JSON parser, works with Absinthe out of the box
     ]
   end
 
