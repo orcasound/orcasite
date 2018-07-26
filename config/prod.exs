61bf7d837747b812d7a2919005076876f8d3b311
Paul Cretu <paul.s.cretu@gmail.com>
2018-07-27 01:15:03 +0300

    Removed ssl redirect

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :orcasite, OrcasiteWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "orcasite.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "orcasite.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
