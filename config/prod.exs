edfc62b04a0a730a8df0bf32a76a911343618a99
Skander Mzali <skanderm@gmail.com>
2018-08-12 17:59:34 -0400

    Added dynamic endpoint host config

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :orcasite, OrcasiteWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "http", host: "orcasite.herokuapp.com", port: 443],
+  url: [scheme: "http", host: Map.fetch!(System.get_env(), "HOST_URL"), port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
