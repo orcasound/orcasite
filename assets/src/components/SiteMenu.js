d9b33c79163f5da7c9f12c8c4ee535fcba8dfe7f
vibecastor <vibecastor@gmail.com>
2019-02-02 15:41:32 -0800

    adding additional styling the navbar and body

--- assets/src/components/SiteMenu.js
+++ assets/src/components/SiteMenu.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react"
-import { Paper, Tabs, Tab, Button, Typography } from "@material-ui/core"
+import { Paper, Tabs, Tab, Button, Typography, Grid } from "@material-ui/core"
+//import { NotificationIcon } from "@material-ui/icons"
+//import { Notifications, ArrowDropDown } from "@material-ui/icons"
+
+import NotificationIcon from "@material-ui/icons/Notifications"
+import ArrowDropDown from "@material-ui/icons/ArrowDropDown"
 import { withStyles } from "@material-ui/core/styles"
 
 const styles = theme => ({
@@ -14,9 +19,27 @@ const styles = theme => ({
     fontFamily: 'Roboto-Medium',
     fontSize: 30,
     letterSpacing: 1.07,
-    // marginRight: 203,
+
     paddingLeft: 20,
     paddingTop: 10,
+  },
+  button: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: 10,
+    color: '#ffffff',
+    opacity: .87,
+    textAlign: 'left'
+  },
+  notificationIcon: {
+    fontSize: 16
+  },
+  // navTabs: {
+  //   display: 'flex',
+  //   justifyContent: 'space-evenly'
+  // },
+  label: {
+    fontSize: 14,
+    textAlign: 'center',
   },
 });
 
@@ -37,21 +60,33 @@ export default withStyles(styles)(
     mabyeAbout = () => {
       if (this.aboutTabSelected()) {
         return (
-          <Paper>
-            <p>Welcome to OrcaSound!</p>
-            <p>
+          <Paper gutterBottom>
+            <Typography
+              variant="h6"
+            >Welcome to OrcaSound!
+                </Typography>
+            <Typography
+              component="p"
+              paragraph={true}
+            >
               Learn what orcas sound like. Then listen live for them on underwater
               microphones (hydrophones).
-          </p>
-            <p>
+                </Typography>
+            <Typography
+              component="p"
+              paragraph={true}
+            >
               Let us know when you hear them, or any sound you think is
               interesting! That will help researchers and stewards protect the
               orcas and their environment.
-          </p>
-            <p>
+                </Typography>
+            <Typography
+              component="p"
+              paragraph={true}
+            >
               You can also get notified when our listeners or algorithms detect
               whales at any of our hydrophone locations.
-          </p>
+          </Typography>
           </Paper>
         )
       }
@@ -82,19 +117,40 @@ export default withStyles(styles)(
             variant="contained"
             centered="true"
             fullWidth={true}
-            className="sitemenu-notification-link"
+            className={classes.button}
+          // Do we need this className?  
+          // className="sitemenu-notification-link"
           >
             Get notified when there's whale activity
-        </Button>
+            <NotificationIcon
+              className={classes.notificationIcon}
+            />
+          </Button>
           <Tabs
             value={this.state.value}
+            variant="fullWidth"
             indicatorColor="primary"
             textColor="primary"
             onChange={this.handleChange}
             centered
           >
-            <Tab label="About" />
-            <Tab label="Listen Live" disabled />
+            <Tab
+              label="About"
+              fullWidth={true}
+              classes={{
+                label: classes.label
+              }}
+            />
+            <Tab
+              label="Listen Live"
+              //disabled
+              //icon={<ArrowDropDown />}
+              fullWidth={true}
+              classes={{
+                label: classes.label,
+              }}
+            >
+            </Tab>
           </Tabs>
           {this.mabyeAbout()}
         </Paper>
