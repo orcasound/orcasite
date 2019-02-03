51ce494b1bb8d332b27ca3f0d5ddeac58430cf9b
vibecastor <vibecastor@gmail.com>
2019-02-02 16:10:05 -0800

    adding CssBaseline Mui compoment

--- assets/src/components/HomeV2.js
+++ assets/src/components/HomeV2.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import SiteMenu from "./SiteMenu"
 import { Button, Paper } from "@material-ui/core"
 import { createMuiTheme, MuiThemeProvider, withStyles } from '@material-ui/core/styles'
+import CssBaseline from "@material-ui/core/CssBaseline"
 import { blue, black } from '@material-ui/core/colors'
 import color from "@material-ui/core/colors/indigo";
 
@@ -36,13 +37,17 @@ export default class HomeV2 extends Component {
 
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <Paper
-          square
-        >
-          <SiteMenu />
-        </Paper>
-      </MuiThemeProvider>
+      <>
+        <CssBaseline>
+          <MuiThemeProvider theme={theme}>
+            <Paper
+              square
+            >
+              <SiteMenu />
+            </Paper>
+          </MuiThemeProvider>
+        </CssBaseline>
+      </>
     )
   }
 }
