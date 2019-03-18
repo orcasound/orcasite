4f2b9e4a2ea6c687ed2e13eaf2f8e814a6d786e8
Samantha Berk <sberk10@gmail.com>
2019-03-17 21:45:04 -0700

    added password hashing to create user

--- lib/orcasite/accounts/user.ex
+++ lib/orcasite/accounts/user.ex
@@ -29,6 +29,10 @@ defmodule Orcasite.Accounts.User do
     |> hash_password
   end
 
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+
   defp hash_password(changeset) do
     changeset
   end
