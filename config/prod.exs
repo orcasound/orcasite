745bd116de5a5ebf386ef1ac407b9a2c3f2129bb
Skander Mzali <skanderm@gmail.com>
2019-08-08 16:35:51 +0200

    Fix wrong arity call for System.get_env

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :orcasite, OrcasiteWeb.Endpoint,
 config :orcasite, Orcasite.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE", "10")),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true,
   types: Orcasite.PostgresTypes
 
