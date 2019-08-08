7f97d7bee4c8518ddd47047af863c0ab05457baf
vibecastor <vibecastor@gmail.com>
2019-08-07 23:22:29 -0700

    removing cssbaseline and roboto cdn

--- assets/src/components/HomeV2.js
+++ assets/src/components/HomeV2.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react"
-import { Paper, Hidden, Container, Grid, Box, Button } from "@material-ui/core"
+import { Paper, Hidden, Grid } from "@material-ui/core"
 
 import FeedPageV2 from "./FeedPageV2"
 import PlayerV2 from "./PlayerV2"
-import DetectionDialogV2 from "./DetectionDialogV2"
 import SiteMenu from "./SiteMenu"
 import AboutV2 from "./AboutV2"
 import AudioExamplesV2 from "./AudioExamplesV2"
@@ -55,12 +54,11 @@ export default class HomeV2 extends Component {
 
   render() {
     const { feedSlug } = this.props.match.params
-    //console.log("theme", theme)
+    console.log("theme", theme)
     return (
       <>
         <StylesProvider injectFirst>
           <ThemeProvider theme={theme}>
-            <CssBaseline />
             <Paper square elevation={0}>
               <SiteMenu />
               {!feedSlug && (
@@ -71,16 +69,6 @@ export default class HomeV2 extends Component {
                   justify="flex-start"
                   alignItems="flex-start"
                 >
-                  {/**
-                  <Hidden only="xl">
-                    <Grid item xl={12}>
-                      <Container fixed>
-                        <Box />
-                      </Container>
-                    </Grid>
-                  </Hidden>
-                */}
-
                   <Grid item xs={12} sm={8} md={8} lg={7} xl={7}>
                     <AboutV2 />
                     <AudioExamplesV2 />
