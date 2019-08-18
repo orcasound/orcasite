28eeadc717f805f3b74157b500332945a2b40840
vibecastor <vibecastor@gmail.com>
2019-08-18 12:58:46 -0700

    refactor SiteMenu to functional component

--- assets/src/components/SiteMenu.js
+++ assets/src/components/SiteMenu.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { Link as RouterLink } from "react-router-dom"
 import {
   Paper,
@@ -14,67 +14,58 @@ import {
 import NotificationIcon from "@material-ui/icons/Notifications"
 import FeedList from "./FeedList"
 
-class SiteMenu extends Component {
-  state = {
-    value: 0
+const SiteMenu = () => {
+  const [value, setValue] = useState(0)
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue)
   }
 
-  handleChange = (event, value) => {
-    //console.log(value)
-    this.setState({ value })
-  }
-
-  aboutTabSelected = () => {
-    return this.state.value == 0
-  }
-
-  render() {
-    return (
-      <Paper elevation={0} square>
-        <AppBar position="static" color="inherit">
-          <Toolbar>
-            <Typography component="h1" variant="h1">
-              <Link
-                component={RouterLink}
-                to="/"
-                color="inherit"
-                underline="none"
-                variant="inherit"
-              >
-                <Box ml={1} pt={3}>
-                  Orcasound
-                </Box>
-              </Link>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Button
-          variant="contained"
-          centered="true"
-          fullWidth={true}
-          color="primary"
-        >
-          <Typography component="div">
-            <Box>Get notified when there's whale activity</Box>
+  return (
+    <Paper elevation={0} square>
+      <AppBar position="static" color="inherit">
+        <Toolbar>
+          <Typography component="h1" variant="h1">
+            <Link
+              component={RouterLink}
+              to="/"
+              color="inherit"
+              underline="none"
+              variant="inherit"
+            >
+              <Box ml={1} pt={3}>
+                Orcasound
+              </Box>
+            </Link>
           </Typography>
-          <Box p={0.2}>
-            <NotificationIcon />
-          </Box>
-        </Button>
-        <Tabs
-          centered
-          variant="fullWidth"
-          value={this.state.value}
-          indicatorColor="primary"
-          onChange={this.handleChange}
-          scrollButtons="off"
-        >
-          <Tab label="About" component={RouterLink} to="/" />
-          <Tab label="Listen Live" component={FeedList} />
-        </Tabs>
-      </Paper>
-    )
-  }
+        </Toolbar>
+      </AppBar>
+      <Button
+        variant="contained"
+        centered="true"
+        fullWidth={true}
+        color="primary"
+      >
+        <Typography component="div">
+          <Box>Get notified when there's whale activity</Box>
+        </Typography>
+        <Box p={0.2}>
+          <NotificationIcon />
+        </Box>
+      </Button>
+      <Tabs
+        centered
+        variant="fullWidth"
+        value={value}
+        indicatorColor="primary"
+        onChange={handleChange}
+        scrollButtons="off"
+      >
+        <Tab label="About" component={RouterLink} to="/" />
+        <Tab label="Listen Live" component={FeedList} />
+      </Tabs>
+    </Paper>
+  )
 }
 
 export default SiteMenu
