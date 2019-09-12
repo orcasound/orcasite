542a3773fe4e0b42d6a3d733cbc26ca518848ed1
Skander Mzali <skanderm@gmail.com>
2019-09-12 17:16:16 -0400

    List fields

--- assets/src/components/admin/EditRecord.js
+++ assets/src/components/admin/EditRecord.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Drawer from "@material-ui/core/Drawer"
+import Box from "@material-ui/core/Box"
 
 const displayFields = ({ record }) =>
   Object.keys(record).filter(name => name !== "__typename")
@@ -7,8 +8,16 @@ const displayFields = ({ record }) =>
 export default function EditRecord(props) {
   return (
     <Drawer open={props.open} onClose={props.onClose}>
-      <h1>{props.record["__typename"]}</h1>
-      {displayFields(props).join(", ")}
+      <Box p={3}>
+        <h1>{props.record["__typename"]}</h1>
+        {displayFields(props).map(field => (
+          <div key={field}>
+            <div>{field}</div>
+            <div>{props.record[field]}</div>
+            <div>{typeof props.record[field]}</div>
+          </div>
+        ))}
+      </Box>
     </Drawer>
   )
 }
