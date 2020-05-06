9c15804bea8e65c8f8760bd4e3d9709d57bb7bbd
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-05-06 13:46:47 +0530

    Setup for push notifications

---
+++ assets/src/config.js
@@ -0,0 +1,11 @@
+// node server
+export default {
+        VAPID_KEY: "BFLXpTODBTbqoZCVaVoswb8rXbYjmrnjSsou7T9-EIkT524zKbk6KtpHuQPXeqrxsr6I8VOARg8u_K9Pyh29MUY",
+        API_URL: "http://localhost:9000"
+}
+
+// elixir server
+// export default {
+//     VAPID_KEY: "BEajsXSIf77FOxdFKpaL8RrOINCdLYkcwID9uRV_OCBTS_BwOZ4ybx3qdaHcLiqjc6P0HAYQDP4zS77Hq3gKE7Y",
+//     API_URL: "http://localhost:8085"
+// }
