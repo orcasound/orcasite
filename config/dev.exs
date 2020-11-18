1ef458a00584dd5ea00bb77a90b2a2039b07989a
watcher00090 <james00090@gmail.com>
2020-11-18 12:03:09 -0800

    changed config

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :orcasite, OrcasiteWeb.Endpoint,
-  http: [port: 8080],
+  http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
