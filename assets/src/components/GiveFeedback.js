9012bbe274607ce75c6a634f5ed61259d1f7cb67
Vibecastor <34181491+vibecastor@users.noreply.github.com>
2021-05-13 16:58:00 -0700

    Hack for your Mother UI Updates (#39)
    * adding notification and feedback buttons and media queries
    
    * Updated AppBar with Notification, Feedback and Listen Live Buttons

--- assets/src/components/GiveFeedback.js
+++ assets/src/components/GiveFeedback.js
@@ -1,57 +1,177 @@
 import React from "react"
-import { AppBar, Button, Link, Grid, makeStyles } from "@material-ui/core"
+import NotificationIcon from "@material-ui/icons/Notifications"
+import FeedbackIcon from "@material-ui/icons/Feedback"
 
-const useStyles = makeStyles(theme => ({
+import {
+  AppBar,
+  Button,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Tooltip,
+  Hidden,
+  makeStyles,
+} from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
   appBar: {
     top: "auto",
     bottom: "0",
     backgroundColor: "none",
-    width: "9.25rem"
-  },
-  button: {
-    bottom: 0,
-    background: theme.palette.common.black,
     width: "9.25rem",
+  },
+  notified: {
+    //bottom: 0,
+    width: "9rem",
     height: "3rem",
-    position: "fixed",
-    borderRadius: ".5rem .5rem 0rem 0rem",
-    opacity: ".8",
     "&:hover": {
-      background: theme.palette.common.black
+      background: theme.palette.common.black,
     },
     "&:focus": {
-      outline: "none"
-    }
+      outline: "none",
+    },
+  },
+  feedback: {
+    //bottom: 0,
+    background: "#000000",
+    color: "#ffffff",
+    width: "9rem",
+    height: "3rem",
+    "&:hover": {
+      background: theme.palette.common.black,
+    },
+    "&:focus": {
+      outline: "none",
+    },
+  },
+  mobileNotified: {
+    //bottom: 0,
+    width: "7rem",
+    height: "3rem",
+    "&:hover": {
+      background: theme.palette.common.black,
+    },
+    "&:focus": {
+      outline: "none",
+    },
+  },
+  mobileFeedback: {
+    //bottom: 0,
+    background: "#000000",
+    color: "#ffffff",
+    width: "6rem",
+    height: "3rem",
+    "&:hover": {
+      background: theme.palette.common.black,
+    },
+    "&:focus": {
+      outline: "none",
+    },
   },
   link: {
+    display: "inline-flex",
     "&:hover": {
       textDecoration: "none",
-      color: "#ffffff"
+      color: "#ffffff",
     },
-    "&:active": {
-      color: "#15766b"
+    "&active": {
+      color: "#15766b",
     },
-    "&:visited": {
-      color: "#ffffff"
-    }
-  }
+    "&visited": {
+      color: "#ffffff",
+    },
+  },
+  icon: {
+    marginLeft: ".35rem",
+  },
+  mobileButtonFont: {
+    fontSize: ".8rem",
+  },
 }))
 
 const GiveFeedback = () => {
   const classes = useStyles()
+  const notificationLink = `https://forms.gle/fn1RRK46TMwNBxGn9`
   const feedbackLink = `https://forms.gle/wKpAnxzUh9a5LMfd7`
 
   return (
-    <AppBar position="relative" className={classes.appBar} color="inherit">
-      <Grid
-        container
-        direction="column"
-        alignItems="flex-start"
-        justify="flex-end"
-      >
+    <Grid
+      container
+      direction="row"
+      alignItems="flex-end"
+      justify="flex-start"
+      spacing={1}
+    >
+      {/** --- MOBILE VIEW --- */}
+      <Hidden smUp>
         <Grid item>
           <Button
-            className={classes.button}
+            className={classes.mobileNotified}
+            variant="contained"
+            color="primary"
+          >
+            <Link
+              href={notificationLink}
+              color="inherit"
+              variant="inherit"
+              target="_blank"
+              rel="noopener"
+              rel="noreferrer"
+              className={classes.link}
+            >
+              <Typography variant="button" className={classes.mobileButtonFont}>
+                Notifications
+              </Typography>
+              <Tooltip
+                arrow
+                title={<Typography>Receive Notifications</Typography>}
+              >
+                <Hidden xsUp>
+                  <NotificationIcon className={classes.icon} fontSize="small" />
+                </Hidden>
+              </Tooltip>
+            </Link>
+          </Button>
+        </Grid>
+        <Grid item>
+          <Box ml={{ xs: 0, sm: 0, md: 2, lg: 4 }}>
+            <Button className={classes.mobileFeedback} variant="contained">
+              <Link
+                href={feedbackLink}
+                color="inherit"
+                variant="inherit"
+                target="_blank"
+                rel="noopener"
+                rel="noreferrer"
+                className={classes.link}
+              >
+                <Typography
+                  variant="button"
+                  className={classes.mobileButtonFont}
+                >
+                  Feedback
+                </Typography>
+                <Tooltip
+                  arrow
+                  title={
+                    <Typography>Share Feedback on your experience</Typography>
+                  }
+                >
+                  <Hidden xsUp>
+                    <FeedbackIcon className={classes.icon} fontSize="small" />
+                  </Hidden>
+                </Tooltip>
+              </Link>
+            </Button>
+          </Box>
+        </Grid>
+      </Hidden>
+      {/** --- TABLET -> DESKTOP BREAKPOINT --- */}
+      <Hidden xsDown>
+        <Grid item>
+          <Button
+            className={classes.notified}
             variant="contained"
             color="primary"
           >
@@ -64,12 +184,43 @@ const GiveFeedback = () => {
               rel="noreferrer"
               className={classes.link}
             >
-              Give Feedback
+              <Typography variant="button">Notifications</Typography>
+              <Tooltip
+                arrow
+                title={<Typography>Receive Notifications</Typography>}
+              >
+                <NotificationIcon className={classes.icon} fontSize="small" />
+              </Tooltip>
             </Link>
           </Button>
         </Grid>
-      </Grid>
-    </AppBar>
+        <Grid item>
+          <Box ml={{ xs: 0, sm: 0, md: 2, lg: 4 }}>
+            <Button className={classes.feedback} variant="contained">
+              <Link
+                href={feedbackLink}
+                color="inherit"
+                variant="inherit"
+                target="_blank"
+                rel="noopener"
+                rel="noreferrer"
+                className={classes.link}
+              >
+                <Typography variant="button">Feedback</Typography>
+                <Tooltip
+                  arrow
+                  title={
+                    <Typography>Share Feedback on your experience</Typography>
+                  }
+                >
+                  <FeedbackIcon className={classes.icon} fontSize="small" />
+                </Tooltip>
+              </Link>
+            </Button>
+          </Box>
+        </Grid>
+      </Hidden>
+    </Grid>
   )
 }
 
--- assets/src/components/SiteMenu.js
+++ assets/src/components/SiteMenu.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Link as RouterLink } from "react-router-dom"
 import {
   Paper,
+  Grid,
   AppBar,
   Toolbar,
   Tabs,
@@ -10,45 +11,43 @@ import {
   Typography,
   Box,
   Link,
-  makeStyles
+  makeStyles,
 } from "@material-ui/core"
 import NotificationIcon from "@material-ui/icons/Notifications"
 import FeedList from "./FeedList"
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   button: {
     minWidth: "180px",
     borderRadius: 0,
     "&:hover": {
       textDecoration: "none",
-      color: "#000000"
+      color: "#000000",
     },
     "&:active": {
-      color: "#15766b"
+      color: "#15766b",
     },
     "&:visited": {
-      color: "#000000"
-    }
+      color: "#000000",
+    },
   },
   link: {
     "&:hover": {
       textDecoration: "none",
-      color: "#ffffff"
+      color: "#ffffff",
     },
     "&:active": {
-      color: "#15766b"
+      color: "#15766b",
     },
     "&:visited": {
-      color: "#ffffff"
-    }
-  }
+      color: "#ffffff",
+    },
+  },
 }))
 
 const SiteMenu = () => {
   const classes = useStyles()
   const [value, setValue] = useState("about")
-
-  const notificationDoc = `https://docs.google.com/forms/d/1oYSTa3QeAAG-G_eTxjabrXd264zVARId9tp2iBRWpFs/edit`
 
   const handleChange = (e, newValue) => {
     setValue(newValue)
@@ -58,65 +57,26 @@ const SiteMenu = () => {
     <Paper elevation={0} square>
       <AppBar position="static" color="inherit">
         <Toolbar>
-          <Typography component="h1" variant="h1">
-            <Link
-              component={RouterLink}
-              to="/"
-              color="inherit"
-              underline="none"
-              variant="inherit"
-              className={classes.link}
-            >
-              <Box ml={1} pt={3}>
-                Orcasound
-              </Box>
-            </Link>
-          </Typography>
+          <Grid container justify="space-between" alignItems="center">
+            <Grid item>
+              <Typography component="h1" variant="h1" className={classes.h1}>
+                <Link
+                  component={RouterLink}
+                  to="/"
+                  color="inherit"
+                  underline="none"
+                  variant="inherit"
+                  className={classes.link}
+                >
+                  <Box ml={0} pt={3}>
+                    Orcasound
+                  </Box>
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
         </Toolbar>
       </AppBar>
-      <Button
-        variant="contained"
-        centered="true"
-        fullWidth={true}
-        color="primary"
-        className={classes.button}
-      >
-        <Typography component="div">
-          <Box zIndex={1}>
-            <Link
-              href={notificationDoc}
-              target="_blank"
-              rel="noopener"
-              rel="noreferrer"
-              color="inherit"
-              variant="inherit"
-              className={classes.link}
-            >
-              Get notified when there's whale activity
-            </Link>
-          </Box>
-        </Typography>
-        <Box p={0.2}>
-          <NotificationIcon />
-        </Box>
-      </Button>
-      <Tabs
-        variant="fullWidth"
-        centered
-        value={value}
-        indicatorColor="primary"
-        onChange={handleChange}
-        scrollButtons="off"
-      >
-        <Tab
-          className={classes.button}
-          value="about"
-          label="About"
-          component={RouterLink}
-          to="/"
-        />
-        <Tab value="listen" label="Listen Live" component={FeedList} />
-      </Tabs>
     </Paper>
   )
 }
