be448cf0bcdbac841b98f738652dff03d449e264
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2021-07-18 15:47:25 +0530

    added security to notification_event query and mutation

--- lib/orcasite_web/graphql/resolvers/notification_event.ex
+++ lib/orcasite_web/graphql/resolvers/notification_event.ex
@@ -3,11 +3,11 @@ defmodule OrcasiteWeb.Resolvers.NotificationEvent do
     alias Orcasite.Notifications
     alias Orcasite.Notifications.NotificationEvents
 
-    def create_notification(params, _info) do
+    def create_notification(params,  %{context: %{current_user: %{admin: true}}}) do
         Notifications.create_notification_event(params) 
     end
 
-    def index(_, _) do
+    def index(_,  %{context: %{current_user: %{admin: true}}}) do
         {:ok, Notifications.list_notification_event()}
     end
 end
