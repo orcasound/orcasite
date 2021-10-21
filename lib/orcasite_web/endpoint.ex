9741d56b24c07b2b477371f698fe50ad43124699
Paul Cretu <paul.s.cretu@gmail.com>
2021-10-21 06:12:25 +0000

    Move request parsing plugs to router.ex and disable on nextjs pipeline
    For rationale see
    https://github.com/tallarium/reverse_proxy_plug#usage-in-phoenix

--- lib/orcasite_web/endpoint.ex
+++ lib/orcasite_web/endpoint.ex
@@ -33,15 +33,6 @@ defmodule OrcasiteWeb.Endpoint do
   plug(Plug.RequestId)
   plug(Orcasite.Logger)
 
-  plug(Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
-    pass: ["*/*"],
-    json_decoder: Jason
-  )
-
-  plug(Plug.MethodOverride)
-  plug(Plug.Head)
-
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
--- lib/orcasite_web/router.ex
+++ lib/orcasite_web/router.ex
@@ -1,12 +1,21 @@
 defmodule OrcasiteWeb.Router do
   use OrcasiteWeb, :router
 
-  pipeline :nextjs do
-    plug(:accepts, ["html"])
-    plug(:put_secure_browser_headers)
+  # Set up request parsers here instead of in endpoint.ex because we don't want
+  # them for the :nextjs pipeline, only :browser and :graphql
+  # See https://github.com/tallarium/reverse_proxy_plug#usage-in-phoenix
+  pipeline :parsers do
+    plug(Plug.Parsers,
+      parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
+      pass: ["*/*"],
+      json_decoder: Jason
+    )
+    plug(Plug.MethodOverride)
+    plug(Plug.Head)
   end
 
   pipeline :browser do
+    plug(:parsers)
     plug(:accepts, ["html"])
     plug(:fetch_session)
     plug(:fetch_flash)
@@ -14,15 +23,23 @@ defmodule OrcasiteWeb.Router do
     plug(:put_secure_browser_headers)
   end
 
+  pipeline :nextjs do
+    plug(:accepts, ["html"])
+    plug(:put_secure_browser_headers)
+  end
+
   pipeline :authorized do
+    plug(:parsers)
     plug(OrcasiteWeb.Auth.AuthAccessPipeline)
   end
 
   pipeline :api do
+    plug(:parsers)
     plug(:accepts, ["json"])
   end
 
   pipeline :graphql do
+    plug(:parsers)
     plug(OrcasiteWeb.Context)
   end
 
