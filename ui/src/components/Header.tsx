cd2ef3b12a4512b1973e345f38589f1a096fc6a2
Paul Cretu <paul.s.cretu@gmail.com>
2021-12-10 03:39:30 +0000

    Make header responsive

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -1,15 +1,58 @@
-import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import MenuIcon from '@mui/icons-material/Menu'
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material'
+import { useState } from 'react'
+
+import useIsMobile from '../hooks/useIsMobile'
 
 export default function Header() {
+  const isMobile = useIsMobile()
   return (
     <Box>
       <AppBar position="sticky">
-        <Toolbar>
-          <Typography variant="h6" sx={{ textTransform: 'uppercase' }}>
-            Orcasound
-          </Typography>
-        </Toolbar>
+        <Toolbar>{isMobile ? <Mobile /> : <Desktop />}</Toolbar>
       </AppBar>
     </Box>
+  )
+}
+
+function Mobile() {
+  const [menuIsOpen, setMenuOpen] = useState(false)
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuIsOpen)
+  }
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 1fr)',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <IconButton
+        sx={{ marginRight: 'auto' }}
+        color="inherit"
+        onClick={handleMenuToggle}
+      >
+        {menuIsOpen ? <CloseIcon /> : <MenuIcon />}
+      </IconButton>
+      <Brand />
+    </Box>
+  )
+}
+
+function Desktop() {
+  return <Brand />
+}
+
+function Brand() {
+  return (
+    <Typography variant="h6" noWrap>
+      Orcasound
+    </Typography>
   )
 }
