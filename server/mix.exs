046f39c57a94fa6c8dc20c060b1990372e81222e
Skander Mzali <skanderm@gmail.com>
2023-04-06 12:22:59 -0700

    Upgrade ohter phoenix deps to 1.15 equivalent

--- server/mix.exs
+++ server/mix.exs
@@ -33,16 +33,16 @@ defmodule Orcasite.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
+      {:phoenix, "~> 1.5.13"},
       {:phoenix_pubsub, "~> 2.0"},
-      {:phoenix_ecto, "~> 4.1"},
+      {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:phoenix_live_view, "~> 0.12.0"},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
-      {:floki, ">= 0.0.0", only: :test},
+      {:phoenix_live_view, "~> 0.15.1"},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:floki, ">= 0.30.0", only: :test},
       {:gettext, "~> 0.11"},
       {:plug_cowboy, "~> 2.1"},
       {:plug, "~> 1.7"},
