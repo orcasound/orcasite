ad2e56ff98758294b2c7e602d4f33e96255d4341
Paul Cretu <paul.s.cretu@gmail.com>
2023-05-10 10:01:46 -0700

    Add category selection to detection button UI (#118)

--- ui/src/components/DetectionDialog.tsx
+++ ui/src/components/DetectionDialog.tsx
@@ -5,22 +5,40 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  Paper,
   TextField,
+  ToggleButton,
+  ToggleButtonGroup,
 } from '@mui/material'
+import Image from 'next/image'
 import { useState } from 'react'
+
+import vesselIconImage from '../../public/icons/vessel-purple.svg'
+import wavesIconImage from '../../public/icons/water-waves-blue.svg'
+import whaleFlukeIconImage from '../../public/icons/whale-fluke-gray.svg'
+
+type DetectionCategory = 'orca' | 'vessel' | 'other'
 
 export default function DetectionDialog(props: any) {
   const [open, setOpen] = useState(false)
   const [submitted, setSubmitted] = useState(false)
+  const [category, setCategory] = useState<DetectionCategory>()
   const [description, setDescription] = useState('')
 
   const handleClickOpen = () => {
     setSubmitted(false)
     setDescription('')
+    setCategory(undefined)
     setOpen(true)
   }
 
   const handleChange = (e: any) => setDescription(e.target.value)
+
+  const handleCategoryChange = (e: any, newCategory: DetectionCategory) => {
+    if (newCategory) {
+      setCategory(newCategory)
+    }
+  }
 
   const handleKeyDown = (submitDetection: any) => (e: any) => {
     if (e.which === 13) {
@@ -65,6 +83,12 @@ export default function DetectionDialog(props: any) {
     onSuccess()
   }
 
+  const categoryButtons = [
+    { id: 'orca', iconImage: whaleFlukeIconImage },
+    { id: 'vessel', iconImage: vesselIconImage },
+    { id: 'other', iconImage: wavesIconImage },
+  ]
+
   return (
     <>
       <Box onClick={handleClickOpen}>{props.children}</Box>
@@ -78,14 +102,52 @@ export default function DetectionDialog(props: any) {
         </DialogTitle>
         {!submitted && (
           <DialogContent>
+            <ToggleButtonGroup
+              value={category}
+              exclusive
+              onChange={handleCategoryChange}
+              size="large"
+              aria-label="Report sound"
+              fullWidth
+              sx={{
+                marginY: 4,
+              }}
+            >
+              {categoryButtons.map(({ id, iconImage }) => (
+                <ToggleButton
+                  value={id}
+                  aria-label={id}
+                  key={id}
+                  component={Paper}
+                  sx={{
+                    '&&&': {
+                      marginX: 5,
+                      borderRadius: 1,
+                      ':hover': {
+                        borderColor: 'primary.main',
+                      },
+                    },
+                    '&&.Mui-selected': {
+                      border: 'solid 2px',
+                      borderColor: 'primary.main',
+                    },
+                  }}
+                >
+                  <DetectionCategoryButton icon={iconImage} title={id} />
+                </ToggleButton>
+              ))}
+            </ToggleButtonGroup>
             <TextField
               autoFocus
               margin="dense"
-              placeholder="Describe what you heard"
+              placeholder="Describe what you heard (optional)"
               type="text"
               fullWidth
               onChange={handleChange}
               onKeyDown={handleKeyDown(submitDetection)}
+              sx={{
+                marginY: 2,
+              }}
             />
           </DialogContent>
         )}
@@ -100,15 +162,13 @@ export default function DetectionDialog(props: any) {
               }}
               color="primary"
               variant="outlined"
+              disabled={!category}
             >
               SUBMIT
             </Button>
           </DialogActions>
         ) : (
           <DialogActions>
-            <Button color="primary" disabled>
-              SHARE
-            </Button>
             <Button onClick={handleClose} color="primary">
               CLOSE
             </Button>
@@ -116,5 +176,26 @@ export default function DetectionDialog(props: any) {
         )}
       </Dialog>
     </>
+  )
+}
+
+function DetectionCategoryButton({
+  icon,
+  title,
+}: {
+  icon: any
+  title: string
+}) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Image src={icon.src} alt={`${title} icon`} width={100} height={100} />
+      {title}
+    </Box>
   )
 }
