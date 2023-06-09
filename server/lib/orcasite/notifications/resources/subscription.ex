72dbbe17023c37920b0dc756b88fd9ac0ebe4844
Skander Mzali <skanderm@gmail.com>
2023-06-08 19:48:00 -0700

    Remove duplicated lines

--- server/lib/orcasite/notifications/resources/subscription.ex
+++ server/lib/orcasite/notifications/resources/subscription.ex
@@ -3,10 +3,9 @@ defmodule Orcasite.Notifications.Subscription do
     extensions: [AshAdmin.Resource, AshAuthentication],
     data_layer: AshPostgres.DataLayer
 
-  alias Orcasite.Notifications.{Event, Notification, SubscriptionNotification, Subscriber}
+  alias Orcasite.Notifications.{Event, Notification, NotificationInstance, Subscriber}
 
   resource do
-    description """
     description """
     A subscription - relates a subscriber to a notification type and a channel.
     (i.e. subscribing to :new_detection via :email)
@@ -20,13 +19,8 @@ defmodule Orcasite.Notifications.Subscription do
 
   actions do
     defaults [:create, :read, :update, :destroy]
-    defaults [:create, :read, :update, :destroy]
 
     create :email_subscribe do
-      description "Create a subscription for an individual email"
-      accept [:event_type, :subscriber, :email, :name]
-      argument :email, :string
-      argument :name, :string
       description "Create a subscription for an individual email"
       accept [:event_type, :subscriber, :email, :name]
       argument :email, :string
@@ -35,17 +29,12 @@ defmodule Orcasite.Notifications.Subscription do
       argument :event_type, :atom do
         constraints one_of: Event.types()
         default nil
-        constraints one_of: Event.types()
-        default nil
       end
 
       argument :subscriber, :uuid
-      argument :subscriber, :uuid
 
       change set_attribute(:event_type, arg(:event_type))
-      change set_attribute(:event_type, arg(:event_type))
 
-      change fn changeset, _context ->
       change fn changeset, _context ->
         changeset
         |> Ash.Changeset.change_attribute(:meta, %{
@@ -91,13 +80,9 @@ defmodule Orcasite.Notifications.Subscription do
 
     update :update_last_notification do
       accept [:last_notification]
-      accept [:last_notification]
 
       argument :last_notification, :uuid
-      argument :last_notification, :uuid
 
-      change manage_relationship(:last_notification, type: :append)
-      change set_attribute(:last_notified_at, &DateTime.utc_now/0)
       change manage_relationship(:last_notification, type: :append)
       change set_attribute(:last_notified_at, &DateTime.utc_now/0)
     end
@@ -109,19 +94,14 @@ defmodule Orcasite.Notifications.Subscription do
       """
 
       pagination keyset?: true, required?: false
-      pagination keyset?: true, required?: false
 
-      argument :notification_id, :uuid
       argument :notification_id, :uuid
 
       argument :event_type, :atom do
         constraints one_of: Event.types()
         default nil
-        constraints one_of: Event.types()
-        default nil
       end
 
-      argument :minutes_ago, :integer, default: 5
       argument :minutes_ago, :integer, default: 5
 
       filter expr(
@@ -139,22 +119,17 @@ defmodule Orcasite.Notifications.Subscription do
 
   code_interface do
     define_for Orcasite.Notifications
-    define_for Orcasite.Notifications
 
-    define :update_last_notification,
     define :update_last_notification,
       action: :update_last_notification,
       args: [:last_notification]
 
-    define :available_for_notification,
     define :available_for_notification,
       action: :available_for_notification,
       args: [:notification_id, :event_type, {:optional, :minutes_ago}]
   end
 
   postgres do
-    table "subscriptions"
-    repo Orcasite.Repo
     table "subscriptions"
     repo Orcasite.Repo
   end
@@ -169,14 +144,10 @@ defmodule Orcasite.Notifications.Subscription do
 
     attribute :event_type, :atom do
       constraints one_of: Event.types()
-      constraints one_of: Event.types()
     end
 
     attribute :last_notified_at, :utc_datetime_usec
-    attribute :last_notified_at, :utc_datetime_usec
 
-    create_timestamp :inserted_at
-    update_timestamp :updated_at
     create_timestamp :inserted_at
     update_timestamp :updated_at
   end
