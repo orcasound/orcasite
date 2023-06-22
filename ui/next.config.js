5bd2c52005dba8d6d2586db4c9d3f6eb74111c94
Paul Cretu <paul.s.cretu@gmail.com>
2023-06-22 20:12:58 +0000

    Install and setup @next/bundle-analyzer

--- ui/next.config.js
+++ ui/next.config.js
@@ -7,4 +7,7 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer(nextConfig)
