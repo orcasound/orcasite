2b2be42aae6d7a918e10a2c4653a16f45d882cac
Paul Cretu <paul.s.cretu@gmail.com>
2023-06-27 07:22:55 +0000

    Refactor player into smaller components

--- ui/src/components/Player.tsx
+++ ui/src/components/Player.tsx
@@ -2,7 +2,7 @@ import { GraphicEq, Pause, Person, PlayArrow } from '@mui/icons-material'
 import { Box, Fab } from '@mui/material'
 import dynamic from 'next/dynamic'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import VideoJSPlayer from 'video.js/dist/types/player'
+import type VideoJSPlayer from 'video.js/dist/types/player'
 
 import DetectionDialog from '@/components/DetectionDialog'
 import type { Feed } from '@/graphql/generated'
@@ -110,106 +110,25 @@ export default function Player({
         position: 'relative',
       }}
     >
+      <Box display="none">
+        <VideoJS options={playerOptions} onReady={handleReady} />
+      </Box>
       {isPlaying && currentFeed && timestamp && (
         <DetectionDialog
           isPlaying={isPlaying}
           feed={currentFeed}
           timestamp={timestamp}
-          getPlayerTime={playerRef.current?.currentTime}
+          getPlayerTime={() => playerRef.current?.currentTime()}
           listenerCount={listenerCount}
         >
-          <Fab
-            variant="extended"
-            size="large"
-            color="secondary"
-            sx={{
-              position: 'absolute',
-              bottom: 100,
-              left: 0,
-              right: 0,
-              margin: 'auto',
-              maxWidth: 'max-content',
-
-              // style to look like outlined button
-              backgroundColor: 'white',
-              color: 'primary.main',
-              borderColor: 'primary.main',
-              borderStyle: 'solid',
-              borderWidth: '2px',
-              '&:hover': {
-                backgroundColor: 'primary.main',
-                color: 'white',
-              },
-            }}
-          >
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <GraphicEq sx={{ mr: 1 }} />
-              Report sound
-            </Box>
-          </Fab>
+          <DetectionButton />
         </DetectionDialog>
       )}
-      <Box
-        sx={{
-          mx: 2,
-          '& .video-js': {
-            display: 'none',
-          },
-        }}
-      >
-        <Fab
-          color="base"
-          sx={{
-            // set hover color manually because custom colors are broken for Fab
-            // see https://github.com/mui/material-ui/issues/31063
-            '&:hover': {
-              backgroundColor: 'base.light',
-            },
-          }}
-          onClick={handlePlayPause}
-        >
-          {!isPlaying && (
-            <PlayArrow
-              className="icon"
-              fontSize="large"
-              // onClick={() =>
-              //   analyticsEvents.stream.started(currentFeed.slug)
-              // }
-            />
-          )}
-          {isPlaying && (
-            <Pause
-              className="icon"
-              fontSize="large"
-              // onClick={() =>
-              //   analyticsEvents.stream.paused(currentFeed.slug)
-              // }
-            />
-          )}
-        </Fab>
-        <VideoJS options={playerOptions} onReady={handleReady} />
+      <Box mx={2}>
+        <PlayPauseButton isPlaying={isPlaying} onClick={handlePlayPause} />
       </Box>
-      <Box
-        sx={{
-          mx: 2,
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        {currentFeed && (
-          <>
-            <Person sx={{ mr: 1 }} />
-            {listenerCount}
-          </>
-        )}
-      </Box>
-      <Box sx={{ mx: 2 }}>
+      <Box mx={2}>{currentFeed && <ListenerCount count={listenerCount} />}</Box>
+      <Box mx={2}>
         {currentFeed
           ? `${currentFeed.name} - ${currentFeed.nodeName}`
           : 'Player: no feed selected'}
@@ -218,6 +137,101 @@ export default function Player({
         {currentFeed &&
           `${currentFeed.locationPoint.coordinates[0]}, ${currentFeed.locationPoint.coordinates[1]}`}
       </Box>
+    </Box>
+  )
+}
+
+function DetectionButton() {
+  return (
+    <Fab
+      variant="extended"
+      size="large"
+      color="secondary"
+      sx={{
+        position: 'absolute',
+        bottom: 100,
+        left: 0,
+        right: 0,
+        margin: 'auto',
+        maxWidth: 'max-content',
+
+        // style to look like outlined button
+        backgroundColor: 'white',
+        color: 'primary.main',
+        borderColor: 'primary.main',
+        borderStyle: 'solid',
+        borderWidth: '2px',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          color: 'white',
+        },
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <GraphicEq sx={{ mr: 1 }} />
+        Report sound
+      </Box>
+    </Fab>
+  )
+}
+
+function PlayPauseButton({
+  isPlaying,
+  onClick,
+}: {
+  isPlaying: boolean
+  onClick: () => void
+}) {
+  return (
+    <Fab
+      color="base"
+      sx={{
+        // set hover color manually because custom colors are broken for Fab
+        // see https://github.com/mui/material-ui/issues/31063
+        '&:hover': {
+          backgroundColor: 'base.light',
+        },
+      }}
+      onClick={onClick}
+    >
+      {!isPlaying && (
+        <PlayArrow
+          className="icon"
+          fontSize="large"
+          // onClick={() =>
+          //   analyticsEvents.stream.started(currentFeed.slug)
+          // }
+        />
+      )}
+      {isPlaying && (
+        <Pause
+          className="icon"
+          fontSize="large"
+          // onClick={() =>
+          //   analyticsEvents.stream.paused(currentFeed.slug)
+          // }
+        />
+      )}
+    </Fab>
+  )
+}
+
+function ListenerCount({ count }: { count: number }) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <Person sx={{ mr: 1 }} />
+      {count}
     </Box>
   )
 }
