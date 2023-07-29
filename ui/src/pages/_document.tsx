7a7875ecbddec670adbd29759433b206d9a382d5
Paul Cretu <paul.s.cretu@gmail.com>
2023-07-29 03:23:01 +0000

    Simplify favicon and line up with pixel grid

--- ui/src/pages/_document.tsx
+++ ui/src/pages/_document.tsx
@@ -31,12 +31,12 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
             prefer svg favicon but use ico as fallback
             svg will change color based on prefers-color-scheme media query
             fallback ico is colorful to provide contrast on both light and dark
-            ico is intentionally not multi-size because otherwise chrome selects it over svg
+            `sizes` attribute is required for Chrome to pick svg over multi-size ico
             (see https://bugs.chromium.org/p/chromium/issues/detail?id=1450857 and
             https://bugs.chromium.org/p/chromium/issues/detail?id=1162276)
         */}
-        <link rel="icon" href="/favicon.ico" sizes="48x48"/>
-        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico?v=2" sizes="48x48"/>
+        <link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml" />
         <meta name="emotion-insertion-point" content="" />
         {emotionStyleTags}
       </Head>
