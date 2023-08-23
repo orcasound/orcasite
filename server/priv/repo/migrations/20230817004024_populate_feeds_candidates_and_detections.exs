1e170df885b0871c3e13c443f3fd6a14172ae21b
Skander Mzali <skanderm@gmail.com>
2023-08-22 17:28:46 -0700

    Remove all transactions/streams

--- server/priv/repo/migrations/20230817004024_populate_feeds_candidates_and_detections.exs
+++ server/priv/repo/migrations/20230817004024_populate_feeds_candidates_and_detections.exs
@@ -7,7 +7,6 @@ defmodule Orcasite.Repo.Migrations.PopulateFeedsCandidatesAndDetections do
 
   def up do
     try do
-
       # Iterate through feeds_legacy, create a new feed
       feed_map =
         from(feed in Orcasite.RadioLegacy.Feed, order_by: [asc: feed.inserted_at])
@@ -42,107 +41,93 @@ defmodule Orcasite.Repo.Migrations.PopulateFeedsCandidatesAndDetections do
         end)
         |> Enum.to_list()
         |> Map.new()
-        |> IO.inspect(label: "feed map? (server/priv/repo/migrations/20230817004024_populate_feeds_candidates_and_detections.exs:#{__ENV__.line})")
 
       # Iterate through candidates_legacy, create a new candidate
-      Repo.transaction(
-        fn ->
-          from(cand in Orcasite.RadioLegacy.Candidate,
-            order_by: [asc: cand.inserted_at],
-            where: not is_nil(cand.min_time) and not is_nil(cand.max_time)
-          )
-          |> Repo.stream()
-          |> Stream.map(fn candidate ->
-            feed_id = Map.get(feed_map, candidate.feed_id)
-            Orcasite.Radio.Candidate
-            |> Ash.Query.for_read(:read)
-            |> Ash.Query.filter(
-              min_time == ^candidate.min_time and max_time == ^candidate.max_time and
-                feed_id == ^feed_id
-            )
-            |> Orcasite.Radio.read()
-            |> case do
-              {:ok, [%Orcasite.Radio.Candidate{} | _]} ->
-                nil
-
-              _ ->
-                Orcasite.Radio.Candidate
-                |> Ash.Changeset.for_create(:create, %{
-                  detection_count: candidate.detection_count,
-                  min_time: candidate.min_time,
-                  max_time: candidate.max_time,
-                  feed: %{id: feed_id}
-                })
-                |> Ash.Changeset.force_change_attributes(%{
-                  inserted_at: candidate.inserted_at,
-                  updated_at: candidate.updated_at
-                })
-                |> Orcasite.Radio.create(return_notifications?: true)
-            end
-          end)
-          |> Stream.run()
-        end,
-        timeout: :infinity
+      from(cand in Orcasite.RadioLegacy.Candidate,
+        order_by: [asc: cand.inserted_at],
+        where: not is_nil(cand.min_time) and not is_nil(cand.max_time)
       )
+      |> Repo.all()
+      |> Enum.map(fn candidate ->
+        feed_id = Map.get(feed_map, candidate.feed_id)
+        Orcasite.Radio.Candidate
+        |> Ash.Query.for_read(:read)
+        |> Ash.Query.filter(
+          min_time == ^candidate.min_time and max_time == ^candidate.max_time and
+            feed_id == ^feed_id
+        )
+        |> Orcasite.Radio.read()
+        |> case do
+          {:ok, [%Orcasite.Radio.Candidate{} | _]} ->
+            nil
+
+          _ ->
+            Orcasite.Radio.Candidate
+            |> Ash.Changeset.for_create(:create, %{
+              detection_count: candidate.detection_count,
+              min_time: candidate.min_time,
+              max_time: candidate.max_time,
+              feed: %{id: feed_id}
+            })
+            |> Ash.Changeset.force_change_attributes(%{
+              inserted_at: candidate.inserted_at,
+              updated_at: candidate.updated_at
+            })
+            |> Orcasite.Radio.create(return_notifications?: true)
+        end
+      end)
 
       # Iterate through each detection_legacy, associate with candidate that matches
       # the timestamp and the feed_id
-      Repo.transaction(
-        fn ->
-          from(det in Orcasite.RadioLegacy.Detection, order_by: [asc: det.inserted_at])
-          |> Repo.stream()
-          |> Stream.map(fn detection ->
-            feed_id = Map.get(feed_map, detection.feed_id)
+      from(det in Orcasite.RadioLegacy.Detection, order_by: [asc: det.inserted_at])
+      |> Repo.all()
+      |> Enum.map(fn detection ->
+        feed_id = Map.get(feed_map, detection.feed_id)
+        Orcasite.Radio.Detection
+        |> Ash.Query.for_read(:read)
+        |> Ash.Query.filter(
+          feed_id == ^feed_id and timestamp == ^detection.timestamp and
+            playlist_timestamp == ^detection.playlist_timestamp and
+            player_offset == ^detection.player_offset
+        )
+        |> Orcasite.Radio.read()
+        |> case do
+          {:ok, [%Orcasite.Radio.Detection{} | _]} ->
+            nil
+
+          _ ->
+            [candidate] =
+              Orcasite.Radio.Candidate
+              |> Ash.Query.for_read(:read)
+              |> Ash.Query.filter(
+                feed_id == ^feed_id and min_time <= ^detection.timestamp and
+                  max_time >= ^detection.timestamp
+              )
+              |> Orcasite.Radio.read!()
+
             Orcasite.Radio.Detection
-            |> Ash.Query.for_read(:read)
-            |> Ash.Query.filter(
-              feed_id == ^feed_id and timestamp == ^detection.timestamp and
-                playlist_timestamp == ^detection.playlist_timestamp and
-                player_offset == ^detection.player_offset
+            |> Ash.Changeset.for_create(
+              :create,
+              Map.take(detection, [
+                :playlist_timestamp,
+                :player_offset,
+                :source_ip,
+                :listener_count,
+                :timestamp,
+                :description
+              ])
+              |> Map.merge(%{
+                candidate: %{id: candidate.id},
+                feed: %{id: feed_id}
+              })
             )
-            |> Orcasite.Radio.read()
-            |> case do
-              {:ok, [%Orcasite.Radio.Detection{} | _]} ->
-                nil
-
-              _ ->
-                [candidate] =
-                  Orcasite.Radio.Candidate
-                  |> Ash.Query.for_read(:read)
-                  |> Ash.Query.filter(
-                    feed_id == ^feed_id and min_time <= ^detection.timestamp and
-                      max_time >= ^detection.timestamp
-                  )
-                  |> Orcasite.Radio.read!()
-
-                Orcasite.Radio.Detection
-                |> Ash.Changeset.for_create(
-                  :create,
-                  Map.take(detection, [
-                    :playlist_timestamp,
-                    :player_offset,
-                    :source_ip,
-                    :listener_count,
-                    :timestamp,
-                    :description
-                  ])
-                  |> Map.merge(%{
-                    candidate: %{id: candidate.id},
-                    feed: %{id: feed_id}
-                  })
-                )
-                |> Ash.Changeset.force_change_attributes(%{
-                  inserted_at: detection.inserted_at,
-                  updated_at: detection.updated_at
-                })
-                |> Orcasite.Radio.create(return_notifications?: true)
-            end
-          end)
-          |> Stream.run()
-        end,
-        timeout: :infinity
-      )
-
+            |> Ash.Changeset.force_change_attributes(%{
+              inserted_at: detection.inserted_at,
+              updated_at: detection.updated_at
+            })
+            |> Orcasite.Radio.create(return_notifications?: true)
+        end
+      end)
     rescue
       err ->
         IO.inspect(err, label: "Error while migrating")
