88e2e4daa3f61de2a44f84fdba1454000b177a74
Skander Mzali <skanderm@gmail.com>
2023-09-06 12:09:15 -0700

    Update populate category script for nil descriptions

--- server/priv/repo/scripts/20230901_populate_detection_category.exs
+++ server/priv/repo/scripts/20230901_populate_detection_category.exs
@@ -2,7 +2,7 @@ Orcasite.Radio.Detection
 |> Ash.Query.for_read(:read)
 |> Orcasite.Radio.read!()
 |> Enum.map(fn detection ->
-  category = case Regex.run(~r/^\[(orca|vessel|other)\]/, detection.description) do
+  category = case Regex.run(~r/^\[(orca|vessel|other)\]/, detection.description || "") do
     [_full, category | _rest ] -> category
     _ -> nil
   end
