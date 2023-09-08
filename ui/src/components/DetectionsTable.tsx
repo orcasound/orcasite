4961a5f2335516f182fc90a7e37ab317ae0554d8
Skander Mzali <skanderm@gmail.com>
2023-09-08 13:46:11 -0700

    Add basic detection table

--- ui/src/components/DetectionsTable.tsx
+++ ui/src/components/DetectionsTable.tsx
@@ -1,13 +1,54 @@
-import { Candidate, Detection, Feed } from "@/graphql/generated";
-import { Box } from "@mui/material";
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
+import { Detection, Feed } from "@/graphql/generated";
+import { formatTimestamp } from "@/utils/time";
 
 export default function DetectionsTable({
   detections,
   feed,
-  candidate
-}: { detections: Detection[], feed: Feed, candidate: Candidate}) {
-  return <Box>
-    {JSON.stringify(detections)}
-  </Box>;
+}: {
+  detections: Detection[];
+  feed: Feed;
+}) {
+  return (
+    <Box>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Node</TableCell>
+            <TableCell>Listeners</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell align="right">Timestamp</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {detections
+            .slice()
+            .sort((a, b) => a.id.localeCompare(b.id))
+            .map((detection) => (
+              <TableRow key={detection.id} hover={true}>
+                <TableCell>{detection.id}</TableCell>
+                <TableCell>{feed.slug}</TableCell>
+                <TableCell>{detection.listenerCount}</TableCell>
+                <TableCell>{detection.description}</TableCell>
+                <TableCell
+                  align="right"
+                  title={detection.timestamp?.toString() || ""}
+                >
+                  {detection.timestamp && formatTimestamp(detection.timestamp)}
+                </TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </Box>
+  );
 }
--- ui/src/pages/submissions/index.tsx
+++ ui/src/pages/submissions/index.tsx
@@ -26,14 +26,11 @@ import {
 import type { NextPageWithLayout } from "@/pages/_app";
 import { formatTimestamp } from "@/utils/time";
 
-// type Unpacked<T> = T extends (infer U)[] ? U : T;
-
 const DetectionsPage: NextPageWithLayout = () => {
   const [rowsPerPage, setRowsPerPage] = useState(50);
   const [page, setPage] = useState(0);
 
   const [detectionModalOpen, setDetectionModalOpen] = useState(false);
-  // const [selectedCandidate, setSelectedCandidate] = useState<Unpacked<Extract<CandidatesQuery, 'candidates'>>>();
   const [selectedCandidate, setSelectedCandidate] = useState<Candidate>();
 
   const candidatesQuery = useCandidatesQuery({
@@ -66,7 +63,7 @@ const DetectionsPage: NextPageWithLayout = () => {
           <Header />
           <Box sx={{ flexGrow: 1, display: "flex" }}>
             <Container>
-              <h1>Detections</h1>
+              <h1>Candidates</h1>
 
               <Paper elevation={1}>
                 <Modal
@@ -74,16 +71,20 @@ const DetectionsPage: NextPageWithLayout = () => {
                   onClose={() => setDetectionModalOpen(false)}
                   className="p-4"
                 >
-                  <Paper classes={{ root: "p-5" }}>
-                    {selectedCandidate && (
-                      <DetectionsTable
-                        candidate={selectedCandidate}
-                        detections={selectedCandidate.detections}
-                        feed={selectedCandidate.feed}
-                      />
-                    )}
-                    {/* <Detections detections={detections} feed={feed} />  */}
-                  </Paper>
+                  <Box p={4}>
+                    <Paper>
+                      <Box p={5}>
+                        <h2>Detections</h2>
+                        {selectedCandidate && (
+                          <DetectionsTable
+                            detections={selectedCandidate.detections}
+                            feed={selectedCandidate.feed}
+                          />
+                        )}
+                      </Box>
+                      {/* <Detections detections={detections} feed={feed} />  */}
+                    </Paper>
+                  </Box>
                 </Modal>
                 <Table>
                   <TableHead>
