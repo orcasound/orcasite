f7f2dcafe6d948957e69d4378c1a29845d7b2ba3
Paul Cretu <paul.s.cretu@gmail.com>
2023-09-27 00:12:36 +0000

    Update landing page

---
+++ ui/src/pages/feeds.tsx
@@ -0,0 +1,54 @@
+import { Container, Stack, Typography } from "@mui/material";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import Head from "next/head";
+
+import FeedCard from "@/components/FeedCard";
+import { getMapLayout } from "@/components/MapLayout";
+import { useFeedsQuery } from "@/graphql/generated";
+import type { NextPageWithLayout } from "@/pages/_app";
+
+const FeedsPage: NextPageWithLayout = () => {
+  const feeds = useFeedsQuery().data?.feeds;
+
+  if (!feeds) return null;
+
+  return (
+    <div>
+      <Head>
+        <title>Orcasound</title>
+      </Head>
+
+      <main>
+        <Container maxWidth="sm">
+          <Typography variant="body1">
+            Select a location to start listening live
+          </Typography>
+          <Stack spacing={4} mt={4}>
+            {feeds.map((feed) => (
+              <FeedCard key={feed.id} feed={feed} />
+            ))}
+          </Stack>
+        </Container>
+      </main>
+    </div>
+  );
+};
+
+FeedsPage.getLayout = getMapLayout;
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    useFeedsQuery.getKey(),
+    useFeedsQuery.fetcher(),
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
+export default FeedsPage;
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,11 +1,27 @@
-import { Container, Stack, Typography } from "@mui/material";
-import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { Box, Container, Typography } from "@mui/material";
 import Head from "next/head";
 
-import FeedCard from "@/components/FeedCard";
 import { getMapLayout } from "@/components/MapLayout";
 import { useFeedsQuery } from "@/graphql/generated";
 import type { NextPageWithLayout } from "@/pages/_app";
+
+const examples = [
+  {
+    title: "Calls",
+    audio:
+      "https://www.orcasound.net/data/product/SRKW/orcasite/call-examples.mp3",
+  },
+  {
+    title: "Clicks",
+    audio:
+      "https://orcasound.net/data/product/SRKW/clicks/20190705-JK_varied_clicks-10sec.mp3",
+  },
+  {
+    title: "Whistles",
+    audio:
+      "https://www.orcasound.net/data/product/SRKW/orcasite/whistle-examples.mp3",
+  },
+];
 
 const HomePage: NextPageWithLayout = () => {
   const feeds = useFeedsQuery().data?.feeds;
@@ -20,14 +36,46 @@ const HomePage: NextPageWithLayout = () => {
 
       <main>
         <Container maxWidth="sm">
-          <Typography variant="body1">
-            Select a location to start listening live
+          <Typography variant="h5" mt={4}>
+            Listen for Whales!
           </Typography>
-          <Stack spacing={4} mt={4}>
-            {feeds.map((feed) => (
-              <FeedCard key={feed.id} feed={feed} />
+          <Typography variant="body1" my={2}>
+            Learn what orcas sound like. Then listen live for them on underwater
+            microphones (hydrophones).
+          </Typography>
+          <Typography variant="body1" my={2}>
+            Let us know when you hear them, or any sound you think is
+            interesting! That will help researchers and stewards protect the
+            orcas and their environment.
+          </Typography>
+          <Typography variant="body1" my={2}>
+            You can also get notified when our listeners or algorithms detect
+            whales at any of our hydrophone locations.
+          </Typography>
+          <Typography variant="h5" mt={4}>
+            What do orcas sound like?
+          </Typography>
+          <Typography variant="body1" my={2}>
+            Here are some samples of calls, clicks, and whistles that are made
+            by southern resident killer whales:
+          </Typography>
+          <Box>
+            {examples.map((example) => (
+              <Box key={example.title} my={2}>
+                <Typography variant="h6">{example.title}</Typography>
+                <Box mt={1}>
+                  <audio
+                    controls
+                    src={example.audio}
+                    // TODO: Re-add analytics
+                    // onPlay={() =>
+                    //   analyticsEvents.about.sampleAudioPlayed(example.title)
+                    // }
+                  />
+                </Box>
+              </Box>
             ))}
-          </Stack>
+          </Box>
         </Container>
       </main>
     </div>
@@ -35,20 +83,5 @@ const HomePage: NextPageWithLayout = () => {
 };
 
 HomePage.getLayout = getMapLayout;
-
-export async function getStaticProps() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery(
-    useFeedsQuery.getKey(),
-    useFeedsQuery.fetcher(),
-  );
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-}
 
 export default HomePage;
