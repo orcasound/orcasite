26223f0296b9feee49c9608cedc79e677787aa1b
Paul Cretu <paul.s.cretu@gmail.com>
2023-09-27 16:57:08 +0000

    Move examples to learn page and add some buttons to landing page

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,27 +1,10 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 import Head from "next/head";
 
+import Link from "@/components/Link";
 import { getMapLayout } from "@/components/MapLayout";
 import { useFeedsQuery } from "@/graphql/generated";
 import type { NextPageWithLayout } from "@/pages/_app";
-
-const examples = [
-  {
-    title: "Calls",
-    audio:
-      "https://www.orcasound.net/data/product/SRKW/orcasite/call-examples.mp3",
-  },
-  {
-    title: "Clicks",
-    audio:
-      "https://orcasound.net/data/product/SRKW/clicks/20190705-JK_varied_clicks-10sec.mp3",
-  },
-  {
-    title: "Whistles",
-    audio:
-      "https://www.orcasound.net/data/product/SRKW/orcasite/whistle-examples.mp3",
-  },
-];
 
 const HomePage: NextPageWithLayout = () => {
   const feeds = useFeedsQuery().data?.feeds;
@@ -52,29 +35,26 @@ const HomePage: NextPageWithLayout = () => {
             You can also get notified when our listeners or algorithms detect
             whales at any of our hydrophone locations.
           </Typography>
-          <Typography variant="h5" mt={4}>
-            What do orcas sound like?
-          </Typography>
-          <Typography variant="body1" my={2}>
-            Here are some samples of calls, clicks, and whistles that are made
-            by southern resident killer whales:
-          </Typography>
-          <Box>
-            {examples.map((example) => (
-              <Box key={example.title} my={2}>
-                <Typography variant="h6">{example.title}</Typography>
-                <Box mt={1}>
-                  <audio
-                    controls
-                    src={example.audio}
-                    // TODO: Re-add analytics
-                    // onPlay={() =>
-                    //   analyticsEvents.about.sampleAudioPlayed(example.title)
-                    // }
-                  />
-                </Box>
-              </Box>
-            ))}
+
+          {/* Buttons for "Listen" and "Learn" */}
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="space-evenly"
+            alignItems="center"
+            mt={5}
+            gap={3}
+          >
+            <Link href={"/learn"} underline="none">
+              <Button variant="outlined" color="secondary" size="large">
+                Learn the sounds
+              </Button>
+            </Link>
+            <Link href={"/feeds"} underline="none">
+              <Button variant="contained" color="primary" size="large">
+                Start listening
+              </Button>
+            </Link>
           </Box>
         </Container>
       </main>
