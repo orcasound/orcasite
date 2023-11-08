1df901723e6450503b6509bc76127d2a655ffacb
DingoDang1113 <Shalpeng109@gmail.com>
2023-11-07 16:43:56 -0800

    registery'

--- server/lib/orcasite/accounts/registry.ex
+++ server/lib/orcasite/accounts/registry.ex
@@ -2,7 +2,7 @@ defmodule Orcasite.Accounts.Registry do
   use Ash.Registry, extensions: [Ash.Registry.ResourceValidations]
 
   entries do
-    entry Orcasite.Accounts.User
-    entry Orcasite.Accounts.Token
+    entry Orcasite.Accounts.User # User resource
+    entry Orcasite.Accounts.Token # Token resource
   end
 end
