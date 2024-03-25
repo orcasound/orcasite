659818d54da87eb081c20bc9363a0f50f63a516f
Paul Cretu <paul.s.cretu@gmail.com>
2024-03-25 20:18:51 +0000

    Update MUI setup to latest example using new @mui/material-nextjs package

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { CacheProvider, EmotionCache } from "@emotion/react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
 import {
   Hydrate,
   QueryClient,
@@ -13,7 +13,6 @@ import Head from "next/head";
 import { ReactElement, ReactNode, useState } from "react";
 import ReactGA from "react-ga4";
 
-import createEmotionCache from "@/styles/createEmotionCache";
 import theme from "@/styles/theme";
 import { GA_TRACKING_ID } from "@/utils/analytics";
 
@@ -21,27 +20,23 @@ export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
-export type MyAppProps = AppProps & {
+type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
-  emotionCache?: EmotionCache;
 };
 
 if (GA_TRACKING_ID) {
   ReactGA.initialize(GA_TRACKING_ID);
 }
 
-// Client-side cache, shared for the whole session of the user in the browser.
-// https://github.com/mui/material-ui/blob/master/examples/material-next-ts/pages/_app.tsx
-const clientSideEmotionCache = createEmotionCache();
+// App needs to be customized in order to make MUI work with SSR
+// https://mui.com/material-ui/integrations/nextjs/#pages-router
+// https://github.com/mui/material-ui/blob/master/examples/material-ui-nextjs-pages-router-ts/pages/_app.tsx
+export default function MyApp(props: AppPropsWithLayout) {
+  const { Component, pageProps } = props;
 
-export default function MyApp({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}: MyAppProps) {
   // Allow pages to define custom per-page layout
-  // Based on https://nextjs.org/docs/basic-features/layouts
-  const getLayout = Component.getLayout || ((page) => page);
+  // Based on https://nextjs.org/docs/pages/building-your-application/routing/pages-and-layouts#with-typescript
+  const getLayout = Component.getLayout ?? ((page) => page);
 
   // Configure react-query using the hydration setup
   // https://react-query.tanstack.com/guides/ssr#using-hydration
@@ -59,7 +54,7 @@ export default function MyApp({
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <CacheProvider value={emotionCache}>
+        <AppCacheProvider {...props}>
           <Head>
             <title>Orcasound</title>
             <meta
@@ -71,7 +66,7 @@ export default function MyApp({
             <CssBaseline />
             {getLayout(<Component {...pageProps} />)}
           </ThemeProvider>
-        </CacheProvider>
+        </AppCacheProvider>
       </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
