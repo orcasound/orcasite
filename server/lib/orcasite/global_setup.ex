7728774060da02014dbc5b5fc5e578aac0af1f2e
Skander Mzali <skanderm@gmail.com>
2024-05-23 20:09:33 -0700

    Simplify stream map

--- server/lib/orcasite/global_setup.ex
+++ server/lib/orcasite/global_setup.ex
@@ -3,16 +3,13 @@ defmodule Orcasite.GlobalSetup do
     Orcasite.Radio.Feed
     |> Ash.Query.for_read(:read)
     |> Orcasite.Radio.read!()
-    |> Task.async_stream(
-      fn feed ->
-        with {:ok, %{timestamps: timestamps}} <- Orcasite.Radio.AwsClient.list_timestamps(feed) do
-          timestamps
-          |> Enum.map(&%{feed: feed, playlist_timestamp: &1})
-          |> Orcasite.Radio.bulk_create(Orcasite.Radio.FeedStream, :create)
-        end
-      end,
-      max_concurrency: 1
-    )
+    |> Stream.map(fn feed ->
+      with {:ok, %{timestamps: timestamps}} <- Orcasite.Radio.AwsClient.list_timestamps(feed) do
+        timestamps
+        |> Enum.map(&%{feed: feed, playlist_timestamp: &1})
+        |> Orcasite.Radio.bulk_create(Orcasite.Radio.FeedStream, :create)
+      end
+    end)
     |> Enum.to_list()
   end
 end
