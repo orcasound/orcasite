e5ecd34efa6531bfa42e11400abbd8d33d0c6ab5
Skander Mzali <skanderm@gmail.com>
2024-10-30 13:15:39 -0500

    Remove outdated url modification

--- server/lib/orcasite_web/json_api_router.ex
+++ server/lib/orcasite_web/json_api_router.ex
@@ -2,13 +2,5 @@ defmodule OrcasiteWeb.JsonApiRouter do
   use AshJsonApi.Router,
     domains: [Orcasite.Notifications, Orcasite.Radio],
     json_schema: "/json_schema",
-    open_api: "/open_api",
-    modify_open_api: {__MODULE__, :modify_open_api, []}
-
-  def modify_open_api(spec, _, _) do
-    %{
-      spec
-      | servers: Enum.map(spec.servers, &%{&1 | url: &1.url <> "/api/json"})
-    }
-  end
+    open_api: "/open_api"
 end
