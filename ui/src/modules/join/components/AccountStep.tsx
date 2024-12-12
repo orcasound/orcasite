72aa0b23dee37c46fff9eb64bb0988df8e719564
Paul Cretu <paul.s.cretu@gmail.com>
2024-12-12 22:54:19 +0000

    Removing password regex requirement

--- ui/src/modules/join/components/AccountStep.tsx
+++ ui/src/modules/join/components/AccountStep.tsx
@@ -21,11 +21,7 @@ const accountSchema = z.object({
   password: z
     .string()
     .min(1, "Password is required")
-    .min(8, "Password must be at least 8 characters")
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
-      "Password must contain at least one uppercase letter, one lowercase letter, and one number",
-    ),
+    .min(8, "Password must be at least 8 characters"),
 });
 
 type AccountFormInputs = z.infer<typeof accountSchema>;
