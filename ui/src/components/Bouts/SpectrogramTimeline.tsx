60b6f07be58e3c8955e818029d80a00a27e0fd60
Skander Mzali <skanderm@gmail.com>
2024-12-17 12:50:56 -0800

    Only show spectrograms for the visible window

--- ui/src/components/Bouts/SpectrogramTimeline.tsx
+++ ui/src/components/Bouts/SpectrogramTimeline.tsx
@@ -4,6 +4,7 @@ import {
   differenceInMilliseconds,
   differenceInMinutes,
   format,
+  subMinutes,
 } from "date-fns";
 import { useEffect, useRef, useState } from "react";
 
@@ -20,6 +21,26 @@ function timeToOffset(
   const diffSeconds = differenceInMilliseconds(time, timelineStartTime) / 1000;
   const pixelsPerSecond = pixelsPerMinute / 60;
   return diffSeconds * pixelsPerSecond;
+}
+
+function offsetToTime(
+  offset: number,
+  timelineStartTime: Date,
+  pixelsPerMinute: number,
+) {
+  return addMinutes(timelineStartTime, offset / pixelsPerMinute);
+}
+
+function rangesOverlap(
+  startTime1?: Date,
+  endTime1?: Date,
+  startTime2?: Date,
+  endTime2?: Date,
+) {
+  if (startTime1 && endTime1 && startTime2 && endTime2) {
+    return startTime1 <= endTime2 && endTime1 >= startTime2;
+  }
+  return false;
 }
 
 function audioImageToUrl({
@@ -50,6 +71,8 @@ export default function SpectrogramTimeline({
   const [spectrogramTime, setSpectrogramTime] = useState<Date>();
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [zoomLevel, setZoomLevel] = useState<number>(10);
+  const [windowStartTime, setWindowStartTime] = useState<Date>();
+  const [windowEndTime, setWindowEndTime] = useState<Date>();
   const minZoom = 5;
   const maxZoom = 100;
 
@@ -66,7 +89,52 @@ export default function SpectrogramTimeline({
       // Set initial spectrogram time
       setSpectrogramTime(playerTime);
     }
-  }, [playerTime, spectrogramTime]);
+    if (
+      windowStartTime === undefined &&
+      windowScrollX.current !== undefined &&
+      timelineStartTime !== undefined
+    ) {
+      setWindowStartTime(
+        offsetToTime(windowScrollX.current, timelineStartTime, pixelsPerMinute),
+      );
+    }
+    if (
+      windowEndTime === undefined &&
+      spectrogramWindow.current !== undefined &&
+      spectrogramWindow.current !== null &&
+      windowScrollX.current !== undefined &&
+      timelineStartTime !== undefined
+    ) {
+      setWindowEndTime(
+        offsetToTime(
+          windowScrollX.current + spectrogramWindow.current.offsetWidth,
+          timelineStartTime,
+          pixelsPerMinute,
+        ),
+      );
+    }
+  }, [
+    playerTime,
+    spectrogramTime,
+    spectrogramWindow,
+    pixelsPerMinute,
+    timelineStartTime,
+    windowStartTime,
+    windowEndTime,
+  ]);
+
+  useEffect(() => {
+    setWindowStartTime(
+      offsetToTime(windowScrollX.current, timelineStartTime, pixelsPerMinute),
+    );
+    setWindowEndTime(
+      offsetToTime(
+        windowScrollX.current + (spectrogramWindow.current?.offsetWidth ?? 0),
+        timelineStartTime,
+        pixelsPerMinute,
+      ),
+    );
+  }, [windowScrollX, spectrogramWindow, timelineStartTime, pixelsPerMinute]);
 
   const handleTouchStart = (e: React.TouchEvent) => {
     setIsDragging(true);
@@ -82,6 +150,16 @@ export default function SpectrogramTimeline({
       e.touches[0].pageX - spectrogramWindow.current.offsetLeft;
     const move = containerCursorX - windowStartX.current;
     spectrogramWindow.current.scrollLeft = windowScrollX.current - move;
+    setWindowStartTime(
+      offsetToTime(windowScrollX.current, timelineStartTime, pixelsPerMinute),
+    );
+    setWindowEndTime(
+      offsetToTime(
+        windowScrollX.current + spectrogramWindow.current.offsetWidth,
+        timelineStartTime,
+        pixelsPerMinute,
+      ),
+    );
   };
 
   const handleMouseDown = (e: React.MouseEvent) => {
@@ -105,13 +183,30 @@ export default function SpectrogramTimeline({
     const containerCursorX = e.pageX - spectrogramWindow.current.offsetLeft;
     const move = containerCursorX - windowStartX.current;
     spectrogramWindow.current.scrollLeft = windowScrollX.current - move;
+    setWindowStartTime(
+      offsetToTime(windowScrollX.current, timelineStartTime, pixelsPerMinute),
+    );
+    setWindowEndTime(
+      offsetToTime(
+        windowScrollX.current + spectrogramWindow.current.offsetWidth,
+        timelineStartTime,
+        pixelsPerMinute,
+      ),
+    );
   };
 
   const handleWheel = (e: React.WheelEvent) => {
     e.preventDefault();
-    setZoomLevel((zoom) =>
-      Math.min(Math.max(minZoom, zoom + e.deltaY * -0.01), maxZoom),
-    );
+    setZoomLevel((zoom) => {
+      const zoomIncrement = zoom * 0.2;
+      return Math.min(
+        Math.max(
+          minZoom,
+          zoom + (e.deltaY > 0 ? -zoomIncrement : zoomIncrement),
+        ),
+        maxZoom,
+      );
+    });
     // if (!spectrogramWindow.current) return;
     // spectrogramWindow.current.scrollLeft -= e.deltaY;
   };
@@ -130,8 +225,11 @@ export default function SpectrogramTimeline({
 
   return (
     <>
-      Start: {JSON.stringify(timelineStartTime)}
-      End: {JSON.stringify(timelineEndTime)}
+      <div>Start: {JSON.stringify(timelineStartTime)}</div>
+      <div>End: {JSON.stringify(timelineEndTime)}</div>
+      <div>Window start: {JSON.stringify(windowStartTime)}</div>
+      <div>Window end: {JSON.stringify(windowEndTime)}</div>
+      <div>Zoom {zoomLevel}</div>
       <Box
         ref={spectrogramWindow}
         position="relative"
@@ -153,6 +251,13 @@ export default function SpectrogramTimeline({
         onTouchMove={handleTouchMove}
         onWheel={handleWheel}
       >
+        {/* <PlayHeadLayer
+          playerTime={playerTime ?? spectrogramTime}
+          timelineStartTime={timelineStartTime}
+          timelineEndTime={timelineEndTime}
+          spectrogramWindow={spectrogramWindow}
+          zIndex={5}
+        /> */}
         <TimelineTickerLayer
           startTime={timelineStartTime}
           endTime={timelineEndTime}
@@ -165,12 +270,16 @@ export default function SpectrogramTimeline({
           pixelsPerMinute={pixelsPerMinute}
           zIndex={1}
         />
-        <FeedSegmentsLayer
-          feedSegments={feedSegments}
-          timelineStartTime={timelineStartTime}
-          pixelsPerMinute={pixelsPerMinute}
-          zIndex={2}
-        />
+        {windowStartTime && windowEndTime && (
+          <FeedSegmentsLayer
+            feedSegments={feedSegments}
+            timelineStartTime={timelineStartTime}
+            pixelsPerMinute={pixelsPerMinute}
+            windowStartTime={windowStartTime}
+            windowEndTime={windowEndTime}
+            zIndex={2}
+          />
+        )}
       </Box>
       ({JSON.stringify(spectrogramTime)} / {JSON.stringify(playerTime)})
     </>
@@ -222,11 +331,15 @@ function FeedSegmentsLayer({
   timelineStartTime,
   pixelsPerMinute,
   zIndex,
+  windowStartTime,
+  windowEndTime,
 }: {
   feedSegments: SpectrogramFeedSegment[];
   timelineStartTime: Date;
   pixelsPerMinute: number;
   zIndex: number;
+  windowStartTime: Date;
+  windowEndTime: Date;
 }) {
   return (
     <>
@@ -234,9 +347,12 @@ function FeedSegmentsLayer({
         if (
           feedSegment.startTime !== undefined &&
           feedSegment.startTime !== null &&
+          feedSegment.endTime !== undefined &&
+          feedSegment.endTime !== null &&
           typeof feedSegment.duration === "string"
         ) {
           const startTime = new Date(feedSegment.startTime);
+          const endTime = new Date(feedSegment.endTime);
           const offset = timeToOffset(
             startTime,
             timelineStartTime,
@@ -257,14 +373,23 @@ function FeedSegmentsLayer({
                 top: TICKER_HEIGHT,
                 width: width,
                 backgroundColor: (theme) => theme.palette.accent2.main,
-                ...(audioImageUrl && {
-                  backgroundImage: `url('${audioImageUrl}')`,
-                  backgroundSize: "cover",
-                }),
+                ...(audioImageUrl &&
+                  rangesOverlap(
+                    subMinutes(startTime, 500 / pixelsPerMinute),
+                    addMinutes(endTime, 500 / pixelsPerMinute),
+                    windowStartTime,
+                    windowEndTime,
+                  ) && {
+                    backgroundImage: `url('${audioImageUrl}')`,
+                    backgroundSize: "cover",
+                  }),
               }}
               display="flex"
               alignItems="center"
               justifyContent="center"
+              data-starttime={feedSegment.startTime}
+              data-endtime={feedSegment.endTime}
+              data-duration={feedSegment.duration}
             >
               <Typography color="white" variant="subtitle1">
                 {!audioImageUrl && startTime?.toLocaleTimeString()}
@@ -290,7 +415,6 @@ function TimelineTickerLayer({
 }) {
   const minutes = differenceInMinutes(endTime, startTime);
   const tiles = minutes; // 1 minute increments
-  const pixelsPerTile = pixelsPerMinute / 6;
   return (
     <>
       {Array(tiles)
@@ -326,22 +450,17 @@ function TimelineTickerLayer({
                 borderLeft="1px solid #666"
                 borderRight="1px solid #aaa"
               />
-              <Box
-                position="absolute"
-                left="25%"
-                bottom="0"
-                height="20%"
-                borderLeft="1px solid #666"
-                borderRight="1px solid #aaa"
-              />
-              <Box
-                position="absolute"
-                left="75%"
-                bottom="0"
-                height="20%"
-                borderLeft="1px solid #666"
-                borderRight="1px solid #aaa"
-              />
+              {[1, 2, 4, 5].map((tensIndex) => (
+                <Box
+                  key={tensIndex}
+                  position="absolute"
+                  left={`${(100 * tensIndex) / 6}%`}
+                  bottom="0"
+                  height="20%"
+                  borderLeft="1px solid #666"
+                  borderRight="1px solid #aaa"
+                />
+              ))}
               <Box
                 position="absolute"
                 left="-50%"
@@ -368,6 +487,35 @@ function TimelineTickerLayer({
             </Box>
           </Box>
         ))}
+    </>
+  );
+}
+
+function PlayHeadLayer({
+  playerTime,
+  timelineStartTime,
+  pixelsPerMinute,
+  zIndex,
+}: {
+  playerTime: Date;
+  // spectrogramWindow: MutableRefObject<HTMLElement | null>;
+  timelineStartTime: Date;
+  pixelsPerMinute: number;
+  zIndex: number;
+}) {
+  const offset = timeToOffset(playerTime, timelineStartTime, pixelsPerMinute);
+
+  return (
+    <>
+      <Box
+        borderLeft="2px solid #eee"
+        position="absolute"
+        left={offset}
+        top={TICKER_HEIGHT}
+        height={`calc(100% - ${TICKER_HEIGHT}px)`}
+        width={1}
+        zIndex={zIndex}
+      ></Box>
     </>
   );
 }
