a412abddc6b292d7a1628e76c1ae47ee193623fa
Adrian <adrmac@gmail.com>
2025-02-06 00:29:19 +0000

    test commit

--- ui/src/pages/playground.tsx
+++ ui/src/pages/playground.tsx
@@ -1,27 +1,26 @@
 import Head from "next/head";
-import { useMemo } from "react";
 
-import { useFeeds2Query } from "@/graphql/generated";
+import { useDetectionsQuery, useFeeds2Query } from "@/graphql/generated";
 import type { NextPageWithLayout } from "@/pages/_app";
 
 const FeedsPage: NextPageWithLayout = () => {
   const feedsQueryResult = useFeeds2Query();
 
-  // Sort feeds by high latitude to low (to match the order on the map)
-  const sortedFeeds = useMemo(
-    () =>
-      feedsQueryResult.data?.feeds.sort((a, b) => b.latLng.lat - a.latLng.lat),
-    [feedsQueryResult.data],
-  );
+  const detectionQueryResult = useDetectionsQuery();
 
-  if (!sortedFeeds) return null;
+  /*
+const recentDetections = detectionQueryResult.data?.detections?.results ?? [];
+const timestamp = recentDetections[0].timestamp;
+const reformatted = new Date(timestamp);
+recentDetections[0] = reformatted.toLocaleDateString(); DONT DO THIS
+*/
 
   return (
     <div>
       <Head>
         <title>Orcasound</title>
       </Head>
-
+      <pre>{JSON.stringify(detectionQueryResult.data, null, 2)}</pre>
       <pre>{JSON.stringify(feedsQueryResult.data, null, 2)}</pre>
     </div>
   );
