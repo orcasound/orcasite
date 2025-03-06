b00464201d051b9bc1028948c7af8ec58caaf479
Adrian <26727606+adrmac@users.noreply.github.com>
2025-03-06 12:41:04 -0800

    Revert "Adrian playground (#787)"
    This reverts commit 1b56c1f83d0b2b2d807eee2ff7202dd247715bc4.

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-"use client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { AppCacheProvider } from "@mui/material-nextjs/v14-pagesRouter";
