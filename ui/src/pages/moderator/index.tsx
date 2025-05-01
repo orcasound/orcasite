bc551bdaadace2804ff721bbfab81d369ded2cf2
Adrian <26727606+adrmac@users.noreply.github.com>
2025-05-01 01:27:52 -0700

    Update index.tsx with layout

--- ui/src/pages/moderator/index.tsx
+++ ui/src/pages/moderator/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPageWithLayout } from "@/pages/_app";
+import { getLeftNavLayout } from "@/components/layouts/LeftNavLayout";
 
 import ModeratorCandidatesPage from "./candidates";
 
 const ModeratorIndexPage: NextPageWithLayout = () => {
   return <ModeratorCandidatesPage />;
 };
+
+ModeratorCandidatesPage.getLayout = getLeftNavLayout;
 
 export default ModeratorIndexPage;
