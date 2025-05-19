e6be303a716293a2b090b3df2bfeafa1f823fc15
Skander Mzali <skanderm@gmail.com>
2025-05-19 11:06:07 -0700

    Add Orcasite.Radio.Seed resource (non-persisted) for interface for seeding resources

---
+++ server/lib/orcasite/radio/seed.ex
@@ -0,0 +1,71 @@
+defmodule Orcasite.Radio.Seed do
+  use Ash.Resource,
+    domain: Orcasite.Radio,
+    extensions: [AshGraphql.Resource]
+
+  resource do
+    description "Non-persisted resource to seed records from specific time ranges from Orcasite prod"
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :resource, :atom do
+      public? true
+      allow_nil? false
+
+      constraints one_of: [
+                    :feed,
+                    :feed_stream,
+                    :feed_segment,
+                    :candidate,
+                    :detection,
+                    :audio_image
+                  ]
+    end
+
+    attribute :start_time, :utc_datetime_usec, allow_nil?: false, public?: true
+    attribute :end_time, :utc_datetime_usec, allow_nil?: false, public?: true
+  end
+
+  validations do
+    validate {Orcasite.Radio.Validations.MaxTimeDiff,
+              [
+                earlier_date_attr: :start_time,
+                later_date_attr: :end_time,
+                max_time: :timer.hours(1)
+              ]}
+
+    validate fn _change, _context ->
+      if Application.get_env(:orcasite, :disable_prod_seed) do
+        {:error, message: "Seeding is disabled (this is likely the production server already)"}
+      else
+        :ok
+      end
+    end
+  end
+
+  actions do
+    create :feeds do
+      accept [:resource, :start_time, :end_time]
+    end
+
+    create :non_feed do
+      accept [:resource, :start_time, :end_time]
+
+      argument :feed_id, :string do
+        allow_nil? false
+        description "Local/dev server feed ID to seed relationship"
+      end
+    end
+  end
+
+  graphql do
+    type :seed
+
+    mutations do
+      create :seed_feeds, :feeds
+      create :seed_non_feed, :non_feed
+    end
+  end
+end
