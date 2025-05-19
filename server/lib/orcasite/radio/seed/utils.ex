6373dc58e0595b7967d9d4432ef1c19b897b6d56
Skander Mzali <skanderm@gmail.com>
2025-05-19 14:38:00 -0700

    Add skeleton for seeding feed streams

--- server/lib/orcasite/radio/seed/utils.ex
+++ server/lib/orcasite/radio/seed/utils.ex
@@ -1,4 +1,10 @@
 defmodule Orcasite.Radio.Seed.Utils do
+  def local_to_prod_feed_id(feed_id) do
+    %{slug: feed_slug} = Orcasite.Radio.Feed |> Ash.get!(feed_id, authorize?: false)
+    %{"id" => id} = Orcasite.Radio.GraphqlClient.get_feed(feed_slug)
+    id
+  end
+
   def prepare_results(results, resource) do
     results
     |> Enum.map(fn result ->
