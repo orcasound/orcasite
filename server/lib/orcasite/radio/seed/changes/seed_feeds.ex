b5bff87f51b4e7c2ecb255ec12e5b3216a1d3eff
Skander Mzali <skanderm@gmail.com>
2025-05-19 12:54:41 -0700

    Add working feed seed

---
+++ server/lib/orcasite/radio/seed/changes/seed_feeds.ex
@@ -0,0 +1,29 @@
+defmodule Orcasite.Radio.Seed.Changes.SeedFeeds do
+  use Ash.Resource.Change
+
+  alias Orcasite.Radio.Seed.Utils
+
+  @impl true
+  def change(changeset, _opts, _context) do
+    changeset
+    |> Ash.Changeset.before_action(fn change ->
+      feeds =
+        Orcasite.Radio.GraphqlClient.get_feeds()
+        |> Utils.prepare_results(Orcasite.Radio.Feed)
+
+      count = Enum.count(feeds)
+
+      feeds
+      |> Ash.bulk_create(Orcasite.Radio.Feed, :create, return_errors?: true, authorize?: false)
+      |> case do
+        %{status: :success} ->
+          change
+          |> Ash.Changeset.force_change_attribute(:seeded, count)
+
+        %{errors: errors} ->
+          change
+          |> Ash.Changeset.add_error(errors)
+      end
+    end)
+  end
+end
