fc04f4ad66e689240a234a899e745801db5f9fec
Skander Mzali <skanderm@gmail.com>
2025-05-26 13:49:18 -0400

    Update seed script, refetch feeds when seeded

--- server/priv/repo/seeds.exs
+++ server/priv/repo/seeds.exs
@@ -1,84 +1,8 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Orcasite.Repo.insert!(%Orcasite.SomeSchema%{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
-#
 require Ash.Query
 
-feeds = [
-  # %{
-  #   lat_lng_string: "48.5583362, -123.1735774",
-  #   name: "Orcasound Lab (Haro Strait)",
-  #   node_name: "rpi_orcasound_lab",
-  #   slug: "orcasound-lab",
-  #   bucket: "dev-streaming-orcasound-net",
-  #   bucket_region: "us-west-2",
-  # },
-  %{
-    lat_lng_string: "47.34922, -122.32512",
-    name: "MaST Center Aquarium",
-    node_name: "rpi_mast_center",
-    slug: "mast-center",
-    bucket: "dev-streaming-orcasound-net",
-    bucket_region: "us-west-2"
-  },
-  # %{
-  #   lat_lng_string: "48.0336664, -122.6040035",
-  #   name: "Bush Point",
-  #   node_name: "rpi_bush_point",
-  #   slug: "bush-point",
-  #   bucket: "dev-streaming-orcasound-net",
-  #   bucket_region: "us-west-2",
-  # },
-  # %{
-  #   lat_lng_string: "48.135743, -122.760614",
-  #   name: "Port Townsend",
-  #   node_name: "rpi_port_townsend",
-  #   slug: "port-townsend",
-  #   bucket: "dev-streaming-orcasound-net",
-  #   bucket_region: "us-west-2",
-  # },
-  %{
-    lat_lng_string: "47.86497296593844, -122.33393605795372",
-    name: "Sunset Bay",
-    node_name: "rpi_sunset_bay",
-    slug: "sunset-bay",
-    bucket: "dev-streaming-orcasound-net",
-    bucket_region: "us-west-2"
-  },
-  %{
-    lat_lng_string: "48.591294, -123.058779",
-    name: "North San Juan Channel",
-    node_name: "rpi_north_sjc",
-    slug: "north-sjc",
-    bucket: "dev-streaming-orcasound-net",
-    bucket_region: "us-west-2"
-  }
-]
+Orcasite.Radio.Seed.feeds()
 
-feeds =
-  for attrs <- feeds do
-    Orcasite.Radio.Feed
-    |> Ash.Query.for_read(:read)
-    |> Ash.Query.filter(slug == ^attrs.slug)
-    |> Ash.read()
-    |> case do
-      {:ok, []} ->
-        Orcasite.Radio.Feed
-        |> Ash.Changeset.for_create(:create, attrs)
-        |> Ash.create!(authorize?: false)
-
-      {:ok, [feed | _]} ->
-        feed
-    end
-  end
+feeds = Orcasite.Radio.Feed |> Ash.read!()
 
 # Create admin account
 strategy = AshAuthentication.Info.strategy!(Orcasite.Accounts.User, :password)
