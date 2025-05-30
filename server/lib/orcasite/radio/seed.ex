37c27ce6a8940feac6baa2b3c82c183e22e4271f
Skander Mzali <skanderm@gmail.com>
2025-05-30 14:59:47 -0700

    Add seedAll

--- server/lib/orcasite/radio/seed.ex
+++ server/lib/orcasite/radio/seed.ex
@@ -15,6 +15,7 @@ defmodule Orcasite.Radio.Seed do
       allow_nil? false
     end
 
+    attribute :feed_id, :string, public?: true
     attribute :start_time, :utc_datetime_usec, allow_nil?: false, public?: true
     attribute :end_time, :utc_datetime_usec, allow_nil?: false, public?: true
     attribute :seeded_count, :integer, public?: true
@@ -23,6 +24,7 @@ defmodule Orcasite.Radio.Seed do
   code_interface do
     define :feeds
     define :resource
+    define :all
   end
 
   validations do
@@ -43,6 +45,44 @@ defmodule Orcasite.Radio.Seed do
   end
 
   actions do
+    defaults [:read]
+
+    action :all, {:array, :struct} do
+      constraints items: [instance_of: __MODULE__]
+      description "Seed feeds, then the rest of the resources"
+      argument :end_time, :utc_datetime_usec, default: &DateTime.utc_now/0
+
+      argument :start_time, :utc_datetime_usec,
+        default: fn -> DateTime.utc_now() |> DateTime.add(-1, :hour) end
+
+      run fn %{arguments: %{start_time: start_time, end_time: end_time}}, _ ->
+        __MODULE__.feeds(%{start_time: start_time, end_time: end_time})
+
+        feeds = Orcasite.Radio.Feed |> Ash.read!()
+
+        seed_params =
+          for feed <- feeds, resource <- [:feed_segment, :detection, :audio_image, :bout] do
+            {feed, resource}
+          end
+
+        seed_params
+        |> Enum.map(fn {feed, resource} -> {feed.slug, resource} end)
+        |> IO.inspect(label: "params")
+
+        seed_params
+        |> Stream.map(fn {feed, resource} ->
+          __MODULE__.resource!(%{
+            resource: resource,
+            start_time: start_time,
+            end_time: end_time,
+            feed_id: feed.id
+          })
+        end)
+        |> Enum.to_list()
+        |> then(&{:ok, &1})
+      end
+    end
+
     create :feeds do
       change set_attribute(:resource, :feed)
       change set_attribute(:start_time, &DateTime.utc_now/0)
@@ -69,6 +109,7 @@ defmodule Orcasite.Radio.Seed do
       change set_attribute(:resource, arg(:resource))
       change set_attribute(:start_time, arg(:start_time))
       change set_attribute(:end_time, arg(:end_time))
+      change set_attribute(:feed_id, arg(:feed_id))
 
       change {__MODULE__.Changes.SeedResource, []}
     end
@@ -80,6 +121,7 @@ defmodule Orcasite.Radio.Seed do
     mutations do
       create :seed_feeds, :feeds
       create :seed_resource, :resource
+      action :seed_all, :all
     end
   end
 end
