a3d3f81dbc7d5af01d1b1f8e794a84994d15278f
Benji <benjifriedman@gmail.com>
2025-06-25 13:53:02 -0700

    Add pagination control to top of table (#849)
    In reference to issue #838
    
    https://github.com/orcasound/orcasite/issues/838

--- ui/src/pages/reports/index.tsx
+++ ui/src/pages/reports/index.tsx
@@ -67,8 +67,30 @@ const DetectionsPage: NextPageWithLayout = () => {
 
       <main>
         <h1>Reports</h1>
-
-        <Paper elevation={1} sx={{ overflow: "auto", mb: 2 }}>
+        
+        <Paper elevation={1} sx={{ overflow: "auto" }}>
+          {candidatesQuery.isSuccess && (
+            <TablePagination
+              component="div"
+              count={candidatesQuery.data?.candidates?.count || 0}
+              page={page}
+              rowsPerPage={rowsPerPage}
+              onPageChange={(e, pg) => {
+                setPage(pg);
+              }}
+              onRowsPerPageChange={(e) => {
+                setRowsPerPage(Number(e.target.value));
+              }}
+              rowsPerPageOptions={[10, 50, 100, 1000]}
+              sx={{
+                borderBottom: 1,
+                borderColor: "divider",
+                display: "flex",
+                justifyContent: "flex-end",
+                alignItems: "center",
+              }}
+            />
+           )}
           <Table>
             <TableHead>
               <TableRow>
